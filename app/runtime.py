04cb74edec2fbf9072df2fad008373a17f1bcd66
smileeyes1 <smile.eyes1@gmail.com>
2026-08-29 12:00:07 +0300

    HAKIM: fix runtime package import

--- app/runtime.py
+++ app/runtime.py
@@ -3,7 +3,7 @@
 from dataclasses import dataclass, field
 from typing import Any, Callable
 
-from capabilities import CapabilityRegistry
+from app.capabilities import CapabilityRegistry
 
 
 @dataclass
